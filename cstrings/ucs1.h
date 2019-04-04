5f36ef80c2ffd9430392499ec2761d4b9fef7500
Kio <kio@little-bat.de>
2019-04-04 19:54:58 +0200

    refactored ucs1; new file unicode.h

--- cstrings/ucs1.h
+++ cstrings/ucs1.h
@@ -25,46 +25,110 @@
 	PERFORMANCE OF THIS SOFTWARE.
 */
 
-#include "Libraries/kio/kio.h"
-#include "Libraries/cstrings/cstrings.h"
-
+#include "cstrings.h"
+#include "unicode.h"
 
 /*
-	namespace ucs1 enhances some functions in cstrings.cpp
-	the functions in this namespace work with 'char' not 'ucs1char'
-	for easier use in normal string handling.
+	UCS-1 characters
 */
 
 namespace ucs1
 {
-static const char replacementchar = '?';	// offiziell $FFFD -> ucs2, utf8 = 3 bytes
+// replacement character for illegal, truncated, unprintable etc. characters
+static const ucs1char replacementchar = '?';	// offiziell $FFFD -> ucs2, utf8 = 3 bytes
 
-extern char const uc_table[0x100];	// simple uppercase table
-extern char const lc_table[0x100];	// simple lowercase table
+using namespace unicode;
+
+// helper:
+extern const uint8 gc_table[256];
+inline bool is_letter (ucs1char c) noexcept;
+
+// get character properies:
+inline Property general_category (ucs1char c) noexcept { return Property(gc_table[c]); }
+inline Property	block_property (ucs1char n) noexcept { return n<0x80 ? U_blk_basic_latin : U_blk_latin_1_supplement; }
+inline Property	script_property	(ucs1char c) noexcept { return is_letter(c) ? U_sc_latin : U_sc_common; }
+inline uint	print_width (ucs1char c) noexcept { return general_category(c) != U_gc_cc; } // 0 or 1
+inline Property	ccc_property (ucs1char) noexcept { return U_ccc_0; }	// for completeness
+inline Property	ea_width_property (ucs1char) noexcept {return U_ea_n; } // for completeness
+inline bool	not_in_any_block (ucs1char) noexcept { return false; }		// for completeness
+
+// helper:
+inline bool is_in_range (ucs1char a, ucs1char c, ucs1char e) noexcept { return uint8(c-a) <= uint8(e-a); }
+inline bool gc_in_range (Property a, ucs1char c, Property e) noexcept { return uint16(general_category(c)-a) <= uint16(e-a); }
+
+
+/* ****************************************************************
+			replicate and extend cstrings functions:
+**************************************************************** */
 
 // is_uppercase() is true for all letters which have a different lowercase letter inside ucs1.
 // is_uppercase() is true for 'A' … 'Z', and 'À' … 'Þ' except '×'.
-//
 // is_lowercase() is true for all letters which have a different uppercase letter inside ucs1 and 'ß' and 'ÿ'.
 // is_lowercase() is true for 'a' … 'z', and 'ß' … 'ÿ' except '÷'.
-//
-// is_letter() is true for all letters which have a different uppercase and lowercase version and for 'ß'.
+// is_letter()    is true for all letters which have a different uppercase and lowercase version and for 'ß'.
 
-inline	bool is_uppercase (char c)	noexcept { return c != lc_table[uchar(c)]; }
-inline	bool is_lowercase (char c)	noexcept { return c != uc_table[uchar(c)] || c==char(0xdf)/*ß*/; }
-inline	bool is_letter (char c)		noexcept { return to_upper(c) != to_lower(c) || c==char(0xdf)/*ß*/; }
+inline bool is_control	 (ucs1char c) noexcept { return general_category(c) == U_gc_cc; } // control code incl. 0x00
+inline bool is_space	 (ucs1char c) noexcept { return (--c&0x7f) <= ' ' || c==0x7e; } // space, nbsp or ctrl excl. 0
+inline bool is_letter	 (ucs1char c) noexcept { return gc_in_range(U_gc_letter,c,U_gc_lu); }
 
-inline	char to_lower (char c)		noexcept { return lc_table[uchar(c)]; }
-inline	char to_upper (char c)		noexcept { return uc_table[uchar(c)]; }
+inline bool is_printable (ucs1char c) noexcept { return general_category(c) != U_gc_cc; }
+inline bool is_lowercase (ucs1char c) noexcept { return general_category(c) == U_gc_ll; }
+inline bool is_uppercase (ucs1char c) noexcept { return general_category(c) == U_gc_lu; }
 
-extern	bool isupperstr	(cstr)		noexcept;
-extern	bool islowerstr	(cstr)		noexcept;
+inline bool is_bin_digit (ucs1char c) noexcept { return (c|1) == '1'; }
+inline bool is_oct_digit (ucs1char c) noexcept { return is_in_range('0',c,'7'); }
+inline bool is_dec_digit (ucs1char c) noexcept { return is_in_range('0',c,'9'); }
+inline bool is_hex_digit (ucs1char c) noexcept { return is_in_range('0',c,'9') || is_in_range('a',c|0x20,'f'); }
 
-inline	void toupper (str s)		throws	{ if(s) for( ;*s;s++ ) *s = to_upper(*s); }
-inline	void tolower (str s)		throws	{ if(s) for( ;*s;s++ ) *s = to_lower(*s); }
-extern	str	 upperstr (cstr)		throws;
-extern	str	 lowerstr (cstr)		throws;
-}
+// digits, indexes, numbers & decorated numbers: 0 - 9, ^2, ^3, ^1, 1/4, 1/2 and 3/4
+inline bool has_numeric_value (ucs1char c) noexcept { return gc_in_range(U_gc_number,c,U_gc_no); }
+
+// decimal digits, roman numbers
+inline bool is_number_letter (ucs1char c) noexcept { return is_dec_digit(c); }
+
+// Simple Upper/Lower/Titlecase:
+inline ucs1char to_lower (ucs1char c) noexcept { return is_uppercase(c) ? c^0x20 : c; }
+inline ucs1char to_upper (ucs1char c) noexcept { return is_uppercase(c^0x20) ? c^0x20 : c; }
+inline ucs1char to_title (ucs1char c) noexcept { return to_upper(c); }
+
+// Get Decimal Digit Value.
+// non-decimal-digits return values ≥ 10.
+// if is_dec_digit() returned true then decimal_digit_value() should return a value in range 0 … 9
+inline uint dec_digit_value (ucs1char c) noexcept { return ::dec_digit_value(char(c)); }
+
+// Get Digit Value in Number Base 2 … 36:
+// non-digits return value ≥ 36.
+inline uint hex_digit_value (ucs1char c) noexcept { return ::hex_digit_value(char(c)); }
+
+// Get Digit, Number & Decorated Number value.
+// everything for which has_numeric_value() returned true. else return NaN
+extern float numeric_value (ucs1char c)	noexcept;
+
+// create hex digit, (masked legal):
+inline ucs1char hexchar (int n) noexcept { return ucs1char(::hexchar(n)); }
+
+
+/* deprecated:
+*/
+inline uint digit_val (ucs2char c) __attribute((deprecated)); // --> dec_digit_value()
+inline uint digit_value (ucs2char c) __attribute((deprecated)); // --> hex_digit_value()
+
+
+/* strings:
+*/
+inline ucs1str tempstr (uint n) noexcept { return ucs1ptr(::tempstr(n)); }
+inline ucs1str tempstr (int  n) noexcept { return ucs1str(::tempstr(n)); }
+inline ucs1str dupstr (ucs1cstr q) noexcept { return ucs1str(::dupstr(cstr(q))); }
+
+extern bool isupperstr (ucs1cstr) noexcept;
+extern bool islowerstr (ucs1cstr) noexcept;
+
+inline void toupper (ucs1str s) noexcept { if(s) for( ;*s;s++ ) *s = to_upper(*s); }
+inline void tolower (ucs1str s) noexcept { if(s) for( ;*s;s++ ) *s = to_lower(*s); }
+extern ucs1str upperstr (ucs1cstr) throws;
+extern ucs1str lowerstr (ucs1cstr) throws;
+
+}; // namespace
 
 
 
