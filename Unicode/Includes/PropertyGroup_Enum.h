321f7251535c35fe41069fcb0ff0106627a9e1bc
Kio <kio@little-bat.de>
2019-04-14 19:02:34 +0200

    refactor Unicode: removed old Unicode/ folder

--- Unicode/Includes/PropertyGroup_Enum.h
+++
@@ -1,9 +0,0 @@
-// property groups, grouped by data type.
-{
-U_numeric_property,
-U_string_property,
-U_miscellaneous_property,
-U_catalog_property,
-U_enumerated_property,
-U_binary_property
-};
--- Unicode/Includes/PropertyGroup_Names.h
+++
@@ -1,9 +0,0 @@
-// names for property groups. matches enum.
-{
-"Numeric Property",
-"String Property",
-"Miscellaneous Property",
-"Catalog Property",
-"Enumerated Property",
-"Binary Property"
-};
