8eaf24a219f880b6d363607ba9c09f59fb717368
kio <kio@little-bat.de>
2019-10-06 19:26:13 +0200

    removed and moved Var/ and VString/ to project vipsi.
    I don't think i'll reuse them in another project.

--- VString/String_errors.h
+++
@@ -1,8 +0,0 @@
-
-
-// to be included in "custom_errors.h":
-
-	EMAC( notastring,		"not a string"					),	// String::StrVal()
-	EMAC( nofinalquotes,	"unterminated text literal"		),	// String::StrVal()
-
-
