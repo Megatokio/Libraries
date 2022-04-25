cd5b5b37a71ebd2b4d201aa9bea0c4031b2fa39d
kio <kio@little-bat.de>
2022-04-25 15:35:29 +0200

    msbit: added template, because, again, overload resolution for int's of same size does not work. it's such a PITA.

--- kio/util/msbit.h
+++ kio/util/msbit.h
@@ -12,18 +12,24 @@
 				msbit(n>0) = int(log(2,n))
 		note:	msbit(n=1) = 0
 		caveat:	msbit(n=0) = 0		// illegal argument!
-
-		note: template does not work correctly because i don't know how to cast a signed type to a same-sized unsigned type
 */
-//INL int msbnibble	(uint8 n) { return (n>7) + (n>3) + (n>1); }										// 0 ..  3
 inline int msbit (uint8  n) noexcept { int b=0,i=4; do{ if(n>>i){n>>=i;b+=i;} }while((i>>=1)); return b; } 	// 0 ..  7
 inline int msbit (uint16 n) noexcept { int b=0,i=8; do{ if(n>>i){n>>=i;b+=i;} }while((i>>=1)); return b; } 	// 0 .. 15
 inline int msbit (uint32 n) noexcept { int b=0,i=16;do{ if(n>>i){n>>=i;b+=i;} }while((i>>=1)); return b; } 	// 0 .. 31
 inline int msbit (uint64 n) noexcept { int b=0,i=32;do{ if(n>>i){n>>=i;b+=i;} }while((i>>=1)); return b; } 	// 0 .. 63
-inline int msbit (int8  n) noexcept { return msbit(uint8(n));  }
-inline int msbit (int16 n) noexcept { return msbit(uint16(n)); }
-inline int msbit (int32 n) noexcept { return msbit(uint32(n)); }
-inline int msbit (int64 n) noexcept { return msbit(uint64(n)); }
+
+// this is a PITA
+#include <type_traits>
+#define only_if_int8  typename std::enable_if<(std::is_integral<T>::value || std::is_enum<T>::value) && sizeof(T)==1,int>::type = 1
+#define only_if_int16 typename std::enable_if<(std::is_integral<T>::value || std::is_enum<T>::value) && sizeof(T)==2,int>::type = 1
+#define only_if_int32 typename std::enable_if<(std::is_integral<T>::value || std::is_enum<T>::value) && sizeof(T)==4,int>::type = 1
+#define only_if_int64 typename std::enable_if<(std::is_integral<T>::value || std::is_enum<T>::value) && sizeof(T)==8,int>::type = 1
+#define only_if_integral_or_enum  typename std::enable_if<(std::is_integral<T>::value || std::is_enum<T>::value),int>::type = 1
+
+template<typename T, only_if_int8> inline int  msbit (T n) noexcept { return msbit(uint8(n));  }
+template<typename T, only_if_int16> inline int msbit (T n) noexcept { return msbit(uint16(n)); }
+template<typename T, only_if_int32> inline int msbit (T n) noexcept { return msbit(uint32(n)); }
+template<typename T, only_if_int64> inline int msbit (T n) noexcept { return msbit(uint64(n)); }
 
 
 /* ----	Calculate the number of digits required to print a number:
@@ -31,8 +37,8 @@ inline int msbit (int64 n) noexcept { return msbit(uint64(n)); }
 				binaryDigits(n=0) = 1
 				binaryDigits(n>0) = ceil(msbit(n+1))
 */
-template <class T> inline int binaryDigits (T number) noexcept { return msbit(number)  +1; }	// result >= 1
-template <class T> inline int hexDigits (T number) noexcept { return msbit(number)/4+1; }	// result >= 1
+template <class T, only_if_integral_or_enum> inline int binaryDigits (T number) noexcept { return msbit(number)  +1; }	// result >= 1
+template <class T, only_if_integral_or_enum> inline int hexDigits (T number) noexcept { return msbit(number)/4+1; }	// result >= 1
 
 
 /* ----	Calculate the number of digits required to store a numbers of a given range:
@@ -43,9 +49,9 @@ template <class T> inline int hexDigits (T number) noexcept { return msbit(numbe
 		note:	reqBits(1) = ceil(msbit(1)) = 0
 		caveat:	reqBits(0) = ceil(msbit(0)) = 0		// illegal range!
 */
-template <class T> inline int reqBits    (T number) noexcept { return number>1 ? msbit(number-1)  +1 : 0; }
-template <class T> inline int reqNibbles (T number) noexcept { return number>1 ? msbit(number-1)/4+1 : 0; }
-template <class T> inline int reqBytes   (T number) noexcept { return number>1 ? msbit(number-1)/8+1 : 0; }
+template <class T, only_if_integral_or_enum> inline int reqBits    (T number) noexcept { return number>1 ? msbit(number-1)  +1 : 0; }
+template <class T, only_if_integral_or_enum> inline int reqNibbles (T number) noexcept { return number>1 ? msbit(number-1)/4+1 : 0; }
+template <class T, only_if_integral_or_enum> inline int reqBytes   (T number) noexcept { return number>1 ? msbit(number-1)/8+1 : 0; }
 
 
 
