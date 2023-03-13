2b02ac23bea550916c639627d074a5ae1dd29c0c
kio <kio@little-bat.de>
2023-03-13 09:56:51 +0100

    QEventTypes: added header file, undo clang-format

---
+++ Qt/QEventTypes.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2009 - 2023 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#pragma once
+#include "kio/kio.h"
+
+
+extern cstr QEventTypeStr(int /*QEvent::Type*/ n);
